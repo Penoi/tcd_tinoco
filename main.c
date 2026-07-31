580bebbc024c4e0f04e3d39e8899ee7d901cb052
yuryturatti <limayury587@gmail.com>
2026-07-31 15:38:53 -0300

    add das bibliotecas de relatorios

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include "ordenar/ordena.h"
 #include "buscar/buscas.h"
-#include "arquivos/arquivos.h"
 #include "relatorios/relatorio.h"
+#include "arquivos/arquivos.h"
 #include <unistd.h>
 
 void menuPrincipal();
