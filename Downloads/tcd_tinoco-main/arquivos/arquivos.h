645b08ddf4c50da76481e022b6600a9836a534f6
Pedro Barbosa <90201059+Penoi@users.noreply.github.com>
2026-08-01 12:27:02 -0300

    Delete Downloads/tcd_tinoco-main directory

--- Downloads/tcd_tinoco-main/arquivos/arquivos.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-/*
-* Lê um arquivo e coloca os numeros dentro de um vetor
-*/
-void lerArquivo(int **vet, char *nome, int *tam); 
-void debug();
--- Downloads/tcd_tinoco-main/tmnc.py
+++
@@ -1,5 +0,0 @@
-import random
-numeros = list(range(1, 20001))
-random.shuffle(numeros)
-with open('numeros.txt', 'w') as f:
-    f.write('\n'.join(map(str, numeros)))
